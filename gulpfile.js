8fb96fad4c89addf442d4fd65d1152d25b1bf416
weilai <tvrcgo@qq.com>
2014-08-27 00:28:03 +0800

    压缩并合并所有产品目录js文件

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var fs = require('fs');
 
 gulp.task('js', function(){
-	gulp.src('app/appname/src/*.js')
-		.pipe(uglify())
-		.pipe(concat('main.js'))
-		.pipe(gulp.dest('app/appname/main.js'));
+
+	var dirs = fs.readdirSync('app');
+	dirs.forEach(function(item){
+		if (fs.statSync('app/'+item).isDirectory()) {
+			gulp.src('app/'+item+'/src/*.js')
+				.pipe(uglify())
+				.pipe(concat('main.js'))
+				.pipe(gulp.dest('app/'+item+'/main.js'));
+		}
+	});
 });
 
 gulp.task('default', ['js']);
