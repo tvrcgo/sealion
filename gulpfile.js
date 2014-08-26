c98283453f6f16b094254e473b53a03ebcb9f586
weilai <tvrcgo@qq.com>
2014-08-27 01:16:13 +0800

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var fs = require('fs');
 
-function traversal(path, ondir, onfile) {
+function walk(path, ondir, onfile) {
 	var dirs = fs.readdirSync(path);
 	dirs.forEach(function(item){
 		if (fs.statSync(path+'/'+item).isDirectory()) {
-			traversal(path+'/'+item, ondir, onfile);
+			walk(path+'/'+item, ondir, onfile);
 			if (typeof ondir === 'function') ondir(path+'/'+item, item);
 		}
 		else {
@@ -19,7 +19,7 @@ function traversal(path, ondir, onfile) {
 
 gulp.task('js', function(){
 
-	traversal('app', function(path, dir){
+	walk('app', function(path, dir){
 		if (fs.existsSync(path+'/src') && dir!=='src') {
 			console.log('concat:', path);
 			gulp.src(path+'/src/*.js')
