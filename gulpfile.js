2110049e4c5aadb8612c54626fb55e2559921908
weilai <tvrcgo@qq.com>
2014-08-27 00:08:02 +0800

    构建配置范例

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require)('gulp-concat');
+
+gulp.task('js', function(){
+	gulp.src('app/appname/src/*.js')
+		.pipe(uglify())
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('app/appname/main.js'));
+});
+
+gulp.task('default', ['js']);
