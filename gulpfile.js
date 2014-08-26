d5bb4cfbe00c9c5d351aa20d7bb48ddde85220cc
weilai <tvrcgo@qq.com>
2014-08-27 01:08:31 +0800

    遍历产品目录合并js

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,31 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var fs = require('fs');
 
-gulp.task('js', function(){
-
-	var dirs = fs.readdirSync('app');
+function traversal(path, ondir, onfile) {
+	var dirs = fs.readdirSync(path);
 	dirs.forEach(function(item){
-		if (fs.statSync('app/'+item).isDirectory()) {
-			gulp.src('app/'+item+'/src/*.js')
-				.pipe(uglify())
-				.pipe(concat('main.js'))
-				.pipe(gulp.dest('app/'+item));
+		if (fs.statSync(path+'/'+item).isDirectory()) {
+			traversal(path+'/'+item, ondir, onfile);
+			if (typeof ondir === 'function') ondir(path+'/'+item, item);
+		}
+		else {
+			if (typeof onfile === 'function') onfile(path+'/'+item, item);
 		}
 	});
+}
+
+gulp.task('js', function(){
+
+	traversal('app', function(path, dir){
+		if (fs.existsSync(path+'/src') && dir!=='src') {
+			console.log('concat:', path);
+			gulp.src(path+'/src/*.js')
+				.pipe(uglify())
+				.pipe(concat('main.js'))
+				.pipe(gulp.dest(path));
+		}
+	});
+
 });
 
 gulp.task('default', ['js']);
